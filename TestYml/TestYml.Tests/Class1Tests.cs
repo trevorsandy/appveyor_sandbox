2b6d91f46ff17eae729b909dc3a8e0799b2d2ea6
Trevor SANDY <trevor.sandy@gmail.com>
2017-04-19 13:12:51 +0200

    First Test
    Initial commit

---
+++ TestYml/TestYml.Tests/Class1Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TestYml
+{
+    public class Class1Tests
+    {
+        [Test]
+        public void TestHello()
+        {
+            var class1 = new Class1();
+
+            Assert.That(class1.Hello(), Is.EqualTo("wat"));
+        }
+    }
+}
